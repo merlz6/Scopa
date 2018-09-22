6abfa77db27a766dba0e16214b90493afc02a84e
Michael Merlo <michaelmerlo@Michaels-MBP.home>
2018-09-22 13:07:16 -0400

    beginning logic , created deck generator

--- js/app.js
+++ js/app.js
@@ -1,6 +1,23 @@
+const suits = ['gold', 'swords', 'vases', 'pots' ]
+const numbers = ['1','2','3','4', '5', '6', '7', '8', '9', '10']
+let player1Cards = [];
+let player2Cards = [];
+let deck = [];
+
+// generate deck makes an array of cards 
+const generateDeck = () => {
+  for(let element of suits){
+    for(let num of numbers){
+      deck.push(numbers[num] + ' of ' + element)
+    }
+  }
+  console.log(deck)
+}
+
+
 $(() => {
 
+  generateDeck();
 
 
-  
 })
